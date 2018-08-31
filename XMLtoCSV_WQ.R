ae1ad6bd40abd5f7130138efe780da4da162aed8
Eric Goodwin <eric.goodwin@cawthron.org.nz>
2018-08-31 16:31:32 +1200

    moved file loading functions to separate functions file

--- XMLtoCSV_WQ.R
+++ XMLtoCSV_WQ.R
@@ -1,217 +1,9 @@
 rm(list=ls())
 library(XML)
-
+source('H:/ericg/16666LAWA/2018/LAWAFunctionsEG.R')
 lawaset=c("NH4", "TURB", "BDISC",  "DRP",  "ECOLI",  "TN",  "TP",  "TON",  "PH")
 
 
-checkCSVage <- function(agency,maxHistory=100){
-  stepBack=0
-  while(stepBack<maxHistory){
-    if(dir.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                         format(Sys.Date()-stepBack,"%Y-%m-%d"),'/'))){
-      if(file.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                            format(Sys.Date()-stepBack,"%Y-%m-%d"),'/',agency,'.csv'))){
-        cat(agency,'from',stepBack,'days ago,',format(Sys.Date()-stepBack,"%Y-%m-%d"),'\n')
-        return(stepBack)
-      }
-    }
-    stepBack=stepBack+1
-  }
-  if(stepBack==maxHistory){
-    cat(agency,'not found within',maxHistory,'days\n')
-    return(maxHistory)
-  }
-}
-checkXMLage <- function(agency,maxHistory=100){
-  stepBack=0
-  while(stepBack<maxHistory){
-    if(dir.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                         format(Sys.Date()-stepBack,"%Y-%m-%d"),'/'))){
-      if(file.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                            format(Sys.Date()-stepBack,"%Y-%m-%d"),'/',agency,'swq.xml'))){
-        cat(agency,'from',stepBack,'days ago,',format(Sys.Date()-stepBack,"%Y-%m-%d"),'\n')
-        return(stepBack)
-      }
-    }
-    stepBack=stepBack+1
-  }
-  if(stepBack==maxHistory){
-    cat(agency,'not found within',maxHistory,'days\n')
-    return(maxHistory)
-  }
-}
-xml2csv <- function(maxHistory=10,quiet=F,reportCensor=F,agency){
-  
-  rm(forcsv)
-  stepBack=0
-  while(stepBack<maxHistory){
-    if(dir.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                         format(Sys.Date()-stepBack,"%Y-%m-%d"),'/'))){
-      if(file.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                            format(Sys.Date()-stepBack,"%Y-%m-%d"),'/',agency,'swq.xml'))){
-        cat('loading',agency,'from',stepBack,'days ago,',format(Sys.Date()-stepBack,"%Y-%m-%d"),'\n')
-        xmlIn <- xmlParse(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                                 format(Sys.Date()-stepBack,"%Y-%m-%d"),'/',agency,'swq.xml'))
-        break
-      }
-    }
-    stepBack=stepBack+1
-  }
-  if(!exists("xmlIn")){
-    cat(agency,'not found, check abbrev. or increase search history\n')
-    return(NULL)
-  }else{
-    varNames = unique(sapply(getNodeSet(doc=xmlIn,path="//Measurement/DataSource"),xmlGetAttr,name='Name'))
-    if("WQ Sample"%in%varNames){
-      cat("\t\tExcluding WQ Sample\n")
-      varNames=varNames[-which(varNames=="WQ Sample")]
-    }
-    siteNames = unique(sapply(getNodeSet(doc=xmlIn,path=paste0("//Measurement")),xmlGetAttr,name='SiteName'))
-    siteNameslc=tolower(siteNames)
-    #WFS returns a list of agencySiteID, SiteID and LawaSiteID
-    #We call the SOS server with agencySiteID and it returns SiteName
-    #So, the SiteNames should match the agencySiteID, right?!
-    cat(length(siteNames),'\n')
-    newRN=1
-    for(sn in 1:length(siteNames)){
-      siteDeets = grep(pattern = siteNameslc[sn],x = siteTable$agencySiteID,ignore.case=T)
-      if(length(siteDeets)==0){
-        cat('A')
-        siteDeets = which(tolower(siteTable$agencySiteID)==tolower(siteNames[sn]))
-      }
-      if(length(siteDeets)>1){
-        cat('B')
-        siteDeets=siteDeets[1]
-      }
-      if(length(siteDeets)!=1){
-        cat('C')
-        siteDeets=which(siteNameslc[sn]==tolower(siteTable$LawaSiteID))
-      }
-      if(length(siteDeets)!=1){
-        cat('D')
-        siteDeets=which(siteNameslc[sn]==tolower(siteTable$SiteID))
-      }
-      if(length(siteDeets)!=1){
-        #This site may have been retired or removed, it didnt come up in the WFS 
-        cat('\t',siteNames[sn],'missing from the WFS feed\n')
-      }
-      
-      for(vn in 1:length(varNames)){
-        dt=sapply(getNodeSet(doc=xmlIn, paste0("//Measurement[@SiteName = '",siteNames[sn],
-                                               "']/DataSource[@Name='",varNames[vn],"']/..//T")), xmlValue)
-        vv=as.character(sapply(getNodeSet(doc=xmlIn, paste0("//Measurement[@SiteName = '",siteNames[sn],
-                                                            "']/DataSource[@Name='",varNames[vn],"']/..//I1")), xmlValue))
-        cens=sapply(getNodeSet(doc=xmlIn, paste0("//Measurement[@SiteName = '",siteNames[sn],
-                                                 "']/DataSource[@Name='",varNames[vn],"']/..//I2")),xmlValue)
-        cenL=grepl(x = cens,pattern = 'ND.<')
-        cenR=grepl(x = cens,'ND.>')
-        if(reportCensor){
-          if(any(cenL)){cat(varNames[vn],'left censored\n')}
-          if(any(cenR)){cat(varNames[vn],'right censored\n')}
-        }
-        if(length(dt)!=length(vv)){
-          browser()
-        }
-        if(length(dt)>0){
-          formattedDate=format.Date(strptime(dt,format="%Y-%m-%dT%H:%M:%S"),"%d-%b-%Y")
-          if(all(is.na(formattedDate))){
-            formattedDate=format.Date(strptime(dt,format="%d/%m/%Y %H:%M"),"%d-%b-%Y")
-          }
-          dtvv=data.frame(SiteName=rep(siteNameslc[sn],length(dt)),
-                          Date=formattedDate,
-                          Value = vv,
-                          Method="",
-                          parameter=varNames[vn],
-                          Censored=F,
-                          CenType=F,
-                          stringsAsFactors = F)
-          rm(formattedDate)
-          if(any(cenL)|any(cenR)){
-            dtvv$Censored[cenL|cenR]=TRUE
-            dtvv$CenType[which(cenL)]="Left"
-            dtvv$CenType[which(cenR)]="Right"
-          }
-          if(length(siteDeets)==1){
-            dtvv=cbind(dtvv,siteTable[siteDeets,])  
-          }
-          eval(parse(text=paste0('dtvv',newRN,'=dtvv')))
-          newRN=newRN+1
-          if(!exists('forcsv')){
-            forcsv=dtvv
-          }else{
-            forcsv=merge(forcsv,dtvv,all=T)
-          }
-        }
-      }
-      if(!quiet){
-        cat('\t',siteNames[sn],'\t',sn,' of ',length(siteNames),'\n')
-      }else{cat('.')}
-    }
-    # forcsv=merge(x=forcsv,y=siteTable,by.x="SiteName",by.y="agencySiteID",all.x=T,suffixes = NULL)
-    forcsv$parameter[tolower(forcsv$parameter) %in% tolower(transfers$nameFrom[tolower(transfers$agency) %in% tolower(agency)])] =
-      transfers$nameTo[tolower(transfers$agency) %in% tolower(agency)][match(x = tolower(forcsv$parameter[tolower(forcsv$parameter) %in%
-                                                                                                             tolower(transfers$nameFrom[tolower(transfers$agency) %in% tolower(agency)])]),
-                                                                              table = tolower(transfers$nameFrom[tolower(transfers$agency) %in% tolower(agency)]))]
-    
-    names(forcsv)[which(names(forcsv)=="SWQFrequencyAll")]="Frequency"
-    excess=unique(forcsv$parameter)[!unique(forcsv$parameter)%in%lawaset]
-    if(length(excess)>0){
-      forcsv=forcsv[-which(forcsv$parameter%in%excess),]
-    }
-    rm(excess)
-    
-    return(forcsv)
-  }
-}
-
-loadLatestCSV <- function(agency=NULL,maxHistory=10){
-  stepBack=0
-  while(stepBack<maxHistory){
-    if(dir.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                         format(Sys.Date()-stepBack,"%Y-%m-%d"),'/'))){
-      if(file.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                            format(Sys.Date()-stepBack,"%Y-%m-%d"),'/',agency,'.csv'))){
-        cat('loading',agency,'from',stepBack,'days ago,',format(Sys.Date()-stepBack,"%Y-%m-%d"),'\n')
-        return(read.csv(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                               format(Sys.Date()-stepBack,"%Y-%m-%d"),'/',agency,'.csv'),
-                        stringsAsFactors = F))
-      }
-    }
-    stepBack=stepBack+1
-  }
-  return(NULL)
-}
-
-checkReturnNames <- function(maxHistory=10,quiet=F,reportCensor=F,agency){
-  #WFS returns a list of agencySiteID, SiteID and LawaSiteID
-  #We call the SOS server with agencySiteID and it returns SiteName
-  #So, the SiteNames should match the agencySiteID, right?!
-  #What proportion do?
-  rm(forcsv)
-  stepBack=0
-  while(stepBack<maxHistory){
-    if(dir.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                         format(Sys.Date()-stepBack,"%Y-%m-%d"),'/'))){
-      if(file.exists(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                            format(Sys.Date()-stepBack,"%Y-%m-%d"),'/',agency,'swq.xml'))){
-        cat('loading',agency,'from',stepBack,'days ago,',format(Sys.Date()-stepBack,"%Y-%m-%d"),'\n')
-        xmlIn <- xmlParse(paste0('h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',
-                                 format(Sys.Date()-stepBack,"%Y-%m-%d"),'/',agency,'swq.xml'))
-        break
-      }
-    }
-    stepBack=stepBack+1
-  }
-  if(!exists("xmlIn")){
-    cat(agency,'not found, check abbrev. or increase search history\n')
-    return(NULL)
-  }else{
-    siteNames = unique(tolower(sapply(getNodeSet(doc=xmlIn,path=paste0("//Measurement")),xmlGetAttr,name='SiteName')))
-    cat(sum(siteNames%in%tolower(siteTable$agencySiteID))/length(siteNames),'\t')
-    cat(sum(siteNames%in%tolower(siteTable$LAWASiteID))/length(siteNames),'\t')
-    cat(sum(siteNames%in%tolower(siteTable$SiteID))/length(siteNames),'\n')
-  }
-}
 # 
 # for(agency in c("boprc","ecan","es","gdc","gwrc","hbrc","hrc","mdc","ncc","nrc","orc","tdc","trc","wcrc","wrc")){
 #   cat('\n',agency,'\n')
@@ -255,82 +47,82 @@ lawaset=c("NH4", "TURB", "BDISC",  "DRP",  "ECOLI",  "TN",  "TP",  "TON",  "PH")
 
 
 #Auckland
-accsv=xml2csv(agency="AC")
+accsv=xml2csvRiver(agency="AC")
 write.csv(accsv,
           file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/ac.csv'),row.names=F)
-# boprccsv$SiteID=ucSIDs[match(boprccsv$agencySiteID,uSIDs)]
+# boprccsv$SiteID=ucSIDs[match(boprccsv$CouncilSiteID,uSIDs)]
 
 #Bay of Plenty
-boprccsv=xml2csv(agency="boprc")
+boprccsv=xml2csvRiver(agency="boprc")
 write.csv(boprccsv,
           file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/boprc.csv'),row.names=F)
 
 #Canterbury
-ecancsv=xml2csv(agency="ECan")
+ecancsv=xml2csvRiver(agency="ECan")
 write.csv(ecancsv,
           file=paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/ecan.csv'),row.names=F)
 
 #Gisborne 
-gdcsv=xml2csv(agency='gdc',maxHistory = 20)
+gdcsv=xml2csvRiver(agency='gdc',maxHistory = 20)
 write.csv(gdcsv,
           file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/gdc.csv'),row.names=F)
 
 #Horizons
-hrccsv=xml2csv(agency="hrc")
+hrccsv=xml2csvRiver(agency="hrc")
 write.csv(hrccsv,
           file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/hrc.csv'),row.names=F)
 
 #Northland 
-nrccsv=xml2csv(agency="nrc")
+nrccsv=xml2csvRiver(agency="nrc")
 write.csv(nrccsv,
           file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/nrc.csv'),row.names=F)
 
 #Taranaki 
-trccsv=xml2csv(agency='TRC')
+trccsv=xml2csvRiver(agency='TRC')
 write.csv(trccsv,
           file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/trc.csv'),row.names=F)
 
 #Hawkes Bay 
-hbrccsv=xml2csv(agency='HBRC')
+hbrccsv=xml2csvRiver(agency='HBRC')
 write.csv(hbrccsv,
           file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/hbrc.csv'),row.names=F)
 
 #Greater Wellington 
-gwrccsv=xml2csv(agency="GWRC")
+gwrccsv=xml2csvRiver(agency="GWRC")
 write.csv(gwrccsv,
           file=paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/gwrc.csv'),row.names=F)
 
 #Nelson
-ncccsv=xml2csv(agency="NCC")
+ncccsv=xml2csvRiver(agency="NCC")
 write.csv(ncccsv,
           file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/ncc.csv'),row.names=F)
 
 #Tasman 
-tdcsv=xml2csv(agency='TDC')
+tdcsv=xml2csvRiver(agency='TDC')
 write.csv(tdcsv,
           file=paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/tdc.csv'),row.names = F)
 
 #Marlborough 
-mdccsv=xml2csv(agency="mdc")
+mdccsv=xml2csvRiver(agency="mdc")
 write.csv(mdccsv,
           file=paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/mdc.csv'),row.names=F)
 
 #Otago 
-orccsv=xml2csv(agency = 'orc')
+orccsv=xml2csvRiver(agency = 'orc')
 write.csv(orccsv,
           file=paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/orc.csv'),row.names=F)
 
 #Environment Southland 
-escsv=xml2csv(agency="ES")
+escsv=xml2csvRiver(agency="ES")
 write.csv(escsv,file =paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/es.csv'),row.names=F)
 
 #West Coast 
-wcrcsv=xml2csv(agency="WCRC")
+wcrcsv=xml2csvRiver(agency="WCRC")
 write.csv(wcrcsv,
           file=paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/wcrc.csv'),row.names=F)
 
 #Waikato
-wrcsv=xml2csv(agency='WRC')
+wrcsv=xml2csvRiver(agency='WRC')
 write.csv(wrcsv,
           file=paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/wrc.csv'),row.names=F)
 
@@ -345,7 +137,7 @@ library(lubridate)
 nms=data.frame(agency=NULL,xmlAge=NULL,var=NULL,earliest=NULL,latest=NULL,nMeas=NULL,nSite=NULL,meanMeas=NULL,maxMeas=NULL,minMeas=NULL,nNA=NULL)
 for(agency in c("ac","boprc","ecan","es","gdc","gwrc","hbrc","hrc","mdc","ncc","nrc","orc","tdc","trc","wcrc","wrc")){
   xmlAge = checkXMLage(agency)
-  mfl=loadLatestCSV(agency)
+  mfl=loadLatestCSVRiver(agency)
   if(!is.null(mfl)){
     newRows=data.frame(agency=rep(agency,length(unique(mfl$parameter))),
                        xmlAge=rep(xmlAge,length(unique(mfl$parameter))),
@@ -378,7 +170,7 @@ write.csv(nms,paste0("h:/ericg/16666LAWA/2018/WaterQuality/QA/WQAudit.csv"),row.
 
 #Per site/measurement start, stop, n and range audit
 for(agency in c("ac","boprc","ecan","es","gdc","gwrc","hbrc","hrc","mdc","ncc","nrc","orc","tdc","trc","wcrc","wrc")){
-  mfl=loadLatestCSV(agency)
+  mfl=loadLatestCSVRiver(agency)
   nvar=length(uvars <- unique(mfl$parameter))
   nsite=length(usites <- unique(mfl$SiteName))
   agencyDeets=as.data.frame(matrix(nrow=nvar*nsite,ncol=7))
@@ -398,7 +190,19 @@ for(agency in c("ac","boprc","ecan","es","gdc","gwrc","hbrc","hrc","mdc","ncc","
   write.csv(agencyDeets,row.names = F,paste0("h:/ericg/16666LAWA/2018/WaterQuality/QA/",agency,"/",agency,"audit.csv"))
 }
 
-
+#Uniquety check
+for(agency in c("ac","boprc","ecan","es","gdc","gwrc","hbrc","hrc","mdc","ncc","nrc","orc","tdc","trc","wcrc","wrc")){
+  mfl=loadLatestCSVRiver(agency)
+  dmfl=dim(mfl)
+  umfl=unique(mfl)
+  dumfl=dim(umfl)
+  if(any(dmfl!=dumfl)){
+    cat(agency,'\t',dmfl,'\t',dumfl,'\n')
+    write.csv(umfl,
+              file=paste0( 'h:/ericg/16666LAWA/2018/WaterQuality/1.Imported/',format(Sys.Date(),"%Y-%m-%d"),'/u',agency,'.csv'),row.names=F)
+    
+    }
+}
 
 
 
